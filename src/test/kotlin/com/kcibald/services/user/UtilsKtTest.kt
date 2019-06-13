ff3889d97a369462280af5ca88ba3beaf20ec759
wusatosi <wusatosi@sina.com>
2019-06-13 17:14:21 +0800

    added default exception fallback message for coroutineHandler

--- src/test/kotlin/com/kcibald/services/user/UtilsKtTest.kt
+++ src/test/kotlin/com/kcibald/services/user/UtilsKtTest.kt
@@ -1,10 +1,8 @@
 package com.kcibald.services.user
 
 import com.kcibald.services.user.handlers.EmptyEventResult
-import io.vertx.core.AsyncResult
-import io.vertx.core.Handler
-import io.vertx.core.MultiMap
-import io.vertx.core.Vertx
+import com.kcibald.services.user.handlers.EventResult
+import io.vertx.core.*
 import io.vertx.core.eventbus.DeliveryOptions
 import io.vertx.core.eventbus.Message
 import io.vertx.core.eventbus.MessageConsumer
@@ -14,6 +12,7 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicReference
 
 @ExtendWith(VertxExtension::class)
 internal class UtilsKtTest {
@@ -28,67 +27,7 @@ internal class UtilsKtTest {
     @Test
     fun coroutineHandler(vertx: Vertx, context: VertxTestContext) {
         val answer = "works!"
-        val target = object : MessageConsumer<String> {
-            override fun bodyStream() = throw IllegalAccessError()
-
-            override fun pause() = throw IllegalAccessError()
-
-            override fun completionHandler(completionHandler: Handler<AsyncResult<Void>>?) = throw IllegalAccessError()
-
-            override fun handler(handler: Handler<Message<String>>): MessageConsumer<String> {
-                handler.handle(object : Message<String> {
-                    override fun replyAddress(): String = throw IllegalAccessError()
-
-                    override fun isSend(): Boolean = throw IllegalAccessError()
-
-                    override fun body(): String = answer
-
-                    override fun address(): String = throw IllegalAccessError()
-
-                    override fun fail(failureCode: Int, message: String?) = throw IllegalAccessError()
-
-                    override fun reply(message: Any?) = throw IllegalAccessError()
-
-                    override fun <R : Any?> reply(
-                        message: Any?,
-                        replyHandler: Handler<AsyncResult<Message<R>>>?
-                    ) = throw IllegalAccessError()
-
-                    override fun reply(message: Any?, options: DeliveryOptions?) = throw IllegalAccessError()
-
-                    override fun <R : Any?> reply(
-                        message: Any?,
-                        options: DeliveryOptions?,
-                        replyHandler: Handler<AsyncResult<Message<R>>>?
-                    ) = throw IllegalAccessError()
-
-                    override fun headers(): MultiMap = throw IllegalAccessError()
-
-                })
-                return this
-            }
-
-            override fun endHandler(endHandler: Handler<Void>?) = throw IllegalAccessError()
-
-            override fun fetch(amount: Long) = throw IllegalAccessError()
-
-            override fun address() = throw IllegalAccessError()
-
-            override fun resume() = throw IllegalAccessError()
-
-            override fun isRegistered() = throw IllegalAccessError()
-
-            override fun unregister() = throw IllegalAccessError()
-
-            override fun unregister(completionHandler: Handler<AsyncResult<Void>>?) = throw IllegalAccessError()
-
-            override fun exceptionHandler(handler: Handler<Throwable>?) = throw IllegalAccessError()
-
-            override fun getMaxBufferedMessages() = throw IllegalAccessError()
-
-            override fun setMaxBufferedMessages(maxBufferedMessages: Int) = throw IllegalAccessError()
-
-        }
+        val target = MockMessageConsumer(answer)
         target.coroutineHandler(vertx) k@{
             context.verify {
                 assertEquals(answer, it.body())
@@ -98,5 +37,102 @@ internal class UtilsKtTest {
         }
         context.awaitCompletion(5, TimeUnit.SECONDS)
     }
+
+    @Test
+    fun coroutineHandlerUnexpectErrorOverride(vertx: Vertx, context: VertxTestContext)  {
+        val answer = "oh yeah"
+        val target = MockMessageConsumer()
+
+        target.coroutineHandler(vertx, object : EventResult {
+            override fun reply(message: Message<*>) {
+                message.reply(answer)
+            }
+        }) {
+            throw IllegalStateException("oh no!")
+        }
+
+        target.completionHandler {
+            context.verify{
+                assertEquals(answer, target.handlerResult.get())
+                context.completeNow()
+            }
+        }
+
+        context.awaitCompletion(5, TimeUnit.SECONDS)
+    }
+
+    class MockMessageConsumer(
+        val body: String = ""
+    ) : MessageConsumer<String> {
+        override fun bodyStream() = throw IllegalAccessError()
+
+        override fun pause() = throw IllegalAccessError()
+
+        private var completionHandlerF: Handler<AsyncResult<Void>>? = null
+
+        override fun completionHandler(completionHandler: Handler<AsyncResult<Void>>?) {
+            this.completionHandlerF = completionHandler
+        }
+
+        var handlerResult: AtomicReference<Any?> = AtomicReference()
+
+        override fun handler(handler: Handler<Message<String>>): MessageConsumer<String> {
+            handler.handle(object : Message<String> {
+                override fun replyAddress(): String = throw IllegalAccessError()
+
+                override fun isSend(): Boolean = throw IllegalAccessError()
+
+                override fun body(): String = body
+
+                override fun address(): String = throw IllegalAccessError()
+
+                override fun fail(failureCode: Int, message: String?) = throw IllegalAccessError()
+
+                override fun reply(message: Any?) {
+                    this@MockMessageConsumer.handlerResult.compareAndSet(null, message)
+                    completionHandlerF?.handle(Future.succeededFuture())
+                }
+
+                override fun <R : Any?> reply(
+                    message: Any?,
+                    replyHandler: Handler<AsyncResult<Message<R>>>?
+                ) = throw IllegalAccessError()
+
+                override fun reply(message: Any?, options: DeliveryOptions?) = throw IllegalAccessError()
+
+                override fun <R : Any?> reply(
+                    message: Any?,
+                    options: DeliveryOptions?,
+                    replyHandler: Handler<AsyncResult<Message<R>>>?
+                ) = throw IllegalAccessError()
+
+                override fun headers(): MultiMap = throw IllegalAccessError()
+
+            })
+            return this
+        }
+
+        override fun endHandler(endHandler: Handler<Void>?) = throw IllegalAccessError()
+
+        override fun fetch(amount: Long) = throw IllegalAccessError()
+
+        override fun address() = throw IllegalAccessError()
+
+        override fun resume() = throw IllegalAccessError()
+
+        override fun isRegistered() = throw IllegalAccessError()
+
+        override fun unregister() = throw IllegalAccessError()
+
+        override fun unregister(completionHandler: Handler<AsyncResult<Void>>?) = throw IllegalAccessError()
+
+        override fun exceptionHandler(handler: Handler<Throwable>?) = throw IllegalAccessError()
+
+        override fun getMaxBufferedMessages() = throw IllegalAccessError()
+
+        override fun setMaxBufferedMessages(maxBufferedMessages: Int) = throw IllegalAccessError()
+
+    }
+
 
 }
