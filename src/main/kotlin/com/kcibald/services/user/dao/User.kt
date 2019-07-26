be9019315337a31a4be9800088016e03ad2819b2
wusatosi <wusatosi@sina.com>
2019-07-26 17:51:08 +0800

    removed unnecessary user

--- src/main/kotlin/com/kcibald/services/user/dao/User.kt
+++ src/main/kotlin/com/kcibald/services/user/dao/User.kt
@@ -3,9 +3,7 @@ package com.kcibald.services.user.dao
 import com.kcibald.services.user.JHelper
 import com.kcibald.services.user.encodeUserIdFromDBID
 import com.kcibald.utils.ImmutableJsonObject
-import io.vertx.core.json.JsonArray
 import io.vertx.core.json.JsonObject
-import java.util.*
 
 internal interface SafeUser {
     val userId: String
@@ -74,78 +72,6 @@ private inline class SafeUserInternal(
         get() =
             jsonObject.getString(avatarFileKey)
 
-}
-
-internal inline class UserInternal(
-    val jsonObject: ImmutableJsonObject
-) : SafeUser {
-    override val userId: String
-        get() = encodeUserIdFromDBID(jsonObject.getString("_id"))
-
-    override val userName: String
-        get() = jsonObject.getString(userNameKey)
-
-    override val urlKey: String
-        get() = jsonObject.getString(urlKeyKey)
-
-    override val signature: String
-        get() =
-            jsonObject.getString(signatureKey)
-
-    override val avatarKey: String
-        get() =
-            jsonObject.getString(avatarFileKey)
-
-    val password: ByteArray
-        get() =
-            Base64
-                .getDecoder()
-                .decode(jsonObject.getString(passwordHashKey))
-
-    val emails: Emails
-        get() = Emails(jsonObject.getJsonArray(emailKey))
-
-}
-
-internal inline class Emails(
-    val jsonArray: JsonArray
-) {
-    val school: EmailConfig
-        get() {
-            for (email in jsonArray) {
-                email as JsonObject
-                val type = email.getString(typeKey)
-                if (type == schoolEmailKey) {
-                    return EmailConfig(email)
-                }
-            }
-            throw AssertionError("school email must be present")
-        }
-
-    val personal: EmailConfig?
-        get() {
-            for (email in jsonArray) {
-                email as JsonObject
-                val type = email.getString(typeKey)
-                if (type == personalEmailKey) {
-                    return EmailConfig(email)
-                }
-            }
-            return null
-        }
-}
-
-internal inline class EmailConfig(
-    val jsonObject: JsonObject
-) {
-    val type: String
-        get() = jsonObject.getString(typeKey)
-
-    val address: String
-        get() = jsonObject.getString(emailAddressKey)
-
-    val verified: Boolean
-        get() = jsonObject.getBoolean(emailVerifiedKey)
 }
 
 internal const val userIdKey = "user_id"
