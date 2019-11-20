6f0397db823215d584ccfbce4918f6c75627f80b
wusatosi <26424577+wusatosi@users.noreply.github.com>
2019-11-20 09:46:11 +0800

    Refactored DescribeUserInterface and added Tests

--- src/main/kotlin/com/kcibald/services/user/handlers/DescribeUserInterface.kt
+++ src/main/kotlin/com/kcibald/services/user/handlers/DescribeUserInterface.kt
@@ -7,11 +7,10 @@ import com.kcibald.services.user.dao.SafeUser
 import com.kcibald.services.user.proto.DescribeUserRequest
 import com.kcibald.services.user.proto.DescribeUserResponse
 import com.kcibald.services.user.proto.Empty
-import com.kcibald.services.user.proto.User
 import com.kcibald.services.user.transform
 import com.kcibald.utils.i
-import io.vertx.core.buffer.Buffer
 import io.vertx.core.eventbus.EventBus
+import io.vertx.core.eventbus.Message
 import io.vertx.core.logging.LoggerFactory
 
 internal class DescribeUserInterface(sharedRuntimeData: SharedRuntimeData) : ServiceInterface(sharedRuntimeData) {
@@ -20,20 +19,22 @@ internal class DescribeUserInterface(sharedRuntimeData: SharedRuntimeData) : Ser
     override suspend fun bind(eventBus: EventBus) {
         val eventBusAddress = runtimeData.config[MasterConfigSpec.DescribeUserConfig.event_bus_name]
         logger.i { "registering Describe User Interface on event bus address: $eventBusAddress" }
-        val consumer = eventBus.consumer<Buffer>(eventBusAddress)
-        consumer.coroutineHandler(runtimeData.vertx, unexpectedErrorEventResult) {
-            val request = DescribeUserRequest.protoUnmarshal(it.body().bytes)
-            return@coroutineHandler try {
-                when (val by = request.by) {
-                    is DescribeUserRequest.By.UrlKey -> packIndividual(queryByURLKey(by.urlKey))
-                    is DescribeUserRequest.By.ID -> packIndividual(queryByID(by.iD))
-                    is DescribeUserRequest.By.UserName -> packMultiple(queryByUserName(by.userName))
-                    else -> throw AssertionError("unreachable branch")
-                }
-            } catch (e: Exception) {
-                logger.warn("unexpected database error, exception: $e", e)
-                databaseErrorEventResult
+        val consumer = eventBus.consumer<ByteArray>(eventBusAddress)
+        consumer.coroutineHandler(runtimeData.vertx, unexpectedErrorEventResult, ::handleEvent)
+    }
+
+    private suspend fun handleEvent(message: Message<ByteArray>): EventResult {
+        val request = DescribeUserRequest.protoUnmarshal(message.body())
+        return try {
+            when (val by = request.by) {
+                is DescribeUserRequest.By.UrlKey -> packIndividual(queryByURLKey(by.urlKey))
+                is DescribeUserRequest.By.ID -> packIndividual(queryByID(by.iD))
+                is DescribeUserRequest.By.UserName -> packMultiple(queryByUserName(by.userName))
+                else -> throw AssertionError("unreachable branch")
             }
+        } catch (e: Exception) {
+            logger.warn("unexpected database error, exception: $e", e)
+            databaseErrorEventResult
         }
     }
 
@@ -43,60 +44,60 @@ internal class DescribeUserInterface(sharedRuntimeData: SharedRuntimeData) : Ser
     private suspend fun queryByID(id: String): SafeUser? =
         runtimeData.dbAccess.getUserWithId(id)
 
-    private suspend fun queryByUserName(userName: String): List<User> {
-        return runtimeData
+    private suspend fun queryByUserName(userName: String): List<SafeUser> =
+        runtimeData
             .dbAccess
             .getUserWithName(userName)
-            .map(SafeUser::transform)
-    }
 
-}
-
-internal val unexpectedErrorEventResult = ProtobufEventResult(
-    DescribeUserResponse(
-        result = DescribeUserResponse.Result.SystemErrorMessage(
-            "unexpected error"
-        )
-    )
-)
-
-internal val databaseErrorEventResult = ProtobufEventResult(
-    DescribeUserResponse(
-        result = DescribeUserResponse.Result.SystemErrorMessage(
-            "database error"
-        )
-    )
-)
-
-internal val userNotFoundEventResult = ProtobufEventResult(
-    DescribeUserResponse(
-        DescribeUserResponse.Result.UserNotFound(Empty())
-    )
-)
-
-internal fun packIndividual(result: SafeUser?): ProtobufEventResult<DescribeUserResponse> {
-    return if (result != null) {
-        ProtobufEventResult(
-            DescribeUserResponse(
-                DescribeUserResponse.Result.SingleUserResult(
-                    DescribeUserResponse.SuccessSingleUserResult(result.transform())
-                )
+    private val unexpectedErrorEventResult = ProtobufEventResult(
+        DescribeUserResponse(
+            result = DescribeUserResponse.Result.SystemErrorMessage(
+                "unexpected error"
             )
         )
-    } else {
-        userNotFoundEventResult
-    }
-}
+    )
 
-internal fun packMultiple(result: List<User>): EventResult {
-    return if (result.isNotEmpty())
-        ProtobufEventResult(
-            DescribeUserResponse(
-                DescribeUserResponse.Result.MultiUserResult(
-                    DescribeUserResponse.SuccessMultiUserResult(result)
-                )
+    private val databaseErrorEventResult = ProtobufEventResult(
+        DescribeUserResponse(
+            result = DescribeUserResponse.Result.SystemErrorMessage(
+                "database error"
             )
         )
-    else
-        userNotFoundEventResult
+    )
+
+    private val userNotFoundEventResult = ProtobufEventResult(
+        DescribeUserResponse(
+            DescribeUserResponse.Result.UserNotFound(Empty())
+        )
+    )
+
+    private fun packIndividual(result: SafeUser?): ProtobufEventResult<DescribeUserResponse> {
+        return if (result != null) {
+            ProtobufEventResult(
+                DescribeUserResponse(
+                    DescribeUserResponse.Result.SingleUserResult(
+                        DescribeUserResponse.SuccessSingleUserResult(result.transform())
+                    )
+                )
+            )
+        } else {
+            userNotFoundEventResult
+        }
+    }
+
+    private fun packMultiple(result: List<SafeUser>): EventResult {
+        return if (result.isNotEmpty()) {
+            val processed = result.map(SafeUser::transform)
+            ProtobufEventResult(
+                DescribeUserResponse(
+                    DescribeUserResponse.Result.MultiUserResult(
+                        DescribeUserResponse.SuccessMultiUserResult(processed)
+                    )
+                )
+            )
+        } else {
+            userNotFoundEventResult
+        }
+    }
+
 }
--- src/test/kotlin/com/kcibald/services/user/handlers/DescribeUserInterfaceKtTest.kt
+++ src/test/kotlin/com/kcibald/services/user/handlers/DescribeUserInterfaceKtTest.kt
@@ -1,142 +1,268 @@
 package com.kcibald.services.user.handlers
 
+import com.kcibald.services.user.MasterConfigSpec
+import com.kcibald.services.user.SharedRuntimeData
+import com.kcibald.services.user.dao.DBAccess
 import com.kcibald.services.user.dao.SafeUser
+import com.kcibald.services.user.genRandomString
+import com.kcibald.services.user.proto.DescribeUserRequest
 import com.kcibald.services.user.proto.DescribeUserResponse
+import com.kcibald.services.user.transform
+import com.uchuhimo.konf.Config
+import io.vertx.core.Vertx
+import io.vertx.core.eventbus.EventBus
+import io.vertx.junit5.VertxExtension
+import io.vertx.kotlin.core.eventbus.requestAwait
+import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(VertxExtension::class)
 class DescribeUserInterfaceKtTest {
 
-    @Test
-    fun unexpectedErrorEventResult() {
-        val message = unexpectedErrorEventResult.message
-        assertTrue(message is DescribeUserResponse)
-        message as DescribeUserResponse
+    private lateinit var vertx: Vertx
+    private lateinit var eventBus: EventBus
+    private val eventBusAddress = "_describe_user_test"
 
-        val result = message.result
-        assertTrue(result is DescribeUserResponse.Result.SystemErrorMessage)
-        result as DescribeUserResponse.Result.SystemErrorMessage
-
-        assertEquals("unexpected error", result.systemErrorMessage)
+    @BeforeEach
+    fun setup(vertx: Vertx) {
+        this.vertx = vertx
+        this.eventBus = vertx.eventBus()
     }
 
     @Test
-    fun databaseErrorEventResult() {
-        val message = databaseErrorEventResult.message
-        assertTrue(message is DescribeUserResponse)
-        message as DescribeUserResponse
-
-        val result = message.result
-        assertTrue(result is DescribeUserResponse.Result.SystemErrorMessage)
-        result as DescribeUserResponse.Result.SystemErrorMessage
-
-        assertEquals("database error", result.systemErrorMessage)
-    }
-
-    @Test
-    fun userNotFoundEventResult() {
-        val message = userNotFoundEventResult.message
-        assertTrue(message is DescribeUserResponse)
-        message as DescribeUserResponse
-
-        val result = message.result
-        assertTrue(result is DescribeUserResponse.Result.UserNotFound)
-    }
-
-    @Test
-    fun packIndividual_not_found() {
-        val packed = packIndividual(null)
-        assertEquals(userNotFoundEventResult, packed)
-    }
-
-    @Test
-    fun packIndividual_normal() {
-        val user = SafeUser(
-            "userId",
-            "user_name",
-            "sig",
-            "avatar"
-        )
-        val packed = packIndividual(user)
-
-        val message = packed.message
-        assertTrue(message is DescribeUserResponse)
-        message as DescribeUserResponse
-
-        val result = message.result
-        assertTrue(result is DescribeUserResponse.Result.SingleUserResult)
-        result as DescribeUserResponse.Result.SingleUserResult
-
-        val resultUser = result.singleUserResult.result!!
-        resultUser.assertEqualsTo(user)
-    }
-
-    @Test
-    fun packMultiple_not_found() {
-        val packed = packMultiple(emptyList())
-        assertEquals(packed, userNotFoundEventResult)
-    }
-
-    @Test
-    fun packMultiple_normal() {
-        val user01 = com.kcibald.services.user.proto.User(
-            "userId",
-            "user_name01",
-            "sig",
-            "avatar"
-        )
-        val user02 = com.kcibald.services.user.proto.User(
-            "userId",
-            "user_name",
-            "sig",
-            "avatar"
-        )
-        val list = ArrayList<com.kcibald.services.user.proto.User>(2)
-        list.add(user01)
-        list.add(user02)
-
-        val packed = packMultiple(
-            list
-        )
-
-        assertTrue(packed is ProtobufEventResult<*>)
-        packed as ProtobufEventResult<*>
-
-        val message = packed.message
-        assertTrue(message is DescribeUserResponse)
-        message as DescribeUserResponse
-        val result = message.result
-        assertTrue(result is DescribeUserResponse.Result.MultiUserResult)
-
-        result as DescribeUserResponse.Result.MultiUserResult
-        result.multiUserResult.result.forEach {
-            assertTrue(list.remove(it))
+    fun describe_user_by_urlKey() = runBlocking {
+        val expectedUser = createTestUser()
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithUrlKey(urlKey: String): SafeUser? {
+                assertEquals(expectedUser.urlKey, urlKey)
+                return expectedUser
+            }
         }
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.UrlKey(expectedUser.urlKey))
+        val response = exchangeRequest(request)
+
+        val result = response.result as DescribeUserResponse.Result.SingleUserResult
+        val user = result.singleUserResult.result ?: fail("no result")
+
+        user.assertEqualsTo(expectedUser)
+
+        Unit
     }
 
     @Test
-    @Disabled("NOT Implemented")
-    fun process_urlKey() {
-        fail<Unit>("NOT Implemented")
+    fun describe_user_by_urlKey_not_found() = runBlocking {
+        val expectedUser: SafeUser? = null
+        val expectedUrlKey = "urlkeyyyyyy"
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithUrlKey(urlKey: String): SafeUser? {
+                assertEquals(expectedUrlKey, urlKey)
+                return expectedUser
+            }
+        }
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.UrlKey(expectedUrlKey))
+        val response = exchangeRequest(request)
+
+        assert(response.result is DescribeUserResponse.Result.UserNotFound)
+
+        Unit
     }
 
     @Test
-    @Disabled("NOT Implemented")
-    fun process_id() {
-        fail<Unit>("NOT Implemented")
+    fun describe_user_by_id() = runBlocking {
+        val expectedUser = createTestUser()
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithId(id: String): SafeUser? {
+                assertEquals(expectedUser.userId, id)
+                return expectedUser
+            }
+        }
+
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.ID(expectedUser.userId))
+        val response = exchangeRequest(request)
+
+        val result = response.result as DescribeUserResponse.Result.SingleUserResult
+        val user = result.singleUserResult.result ?: fail("no result")
+
+        user.assertEqualsTo(expectedUser)
+
+        Unit
     }
 
     @Test
-    @Disabled("NOT Implemented")
-    fun process_username_single() {
-        fail<Unit>("NOT Implemented")
+    fun describe_user_by_id_not_found() = runBlocking {
+        val expectedUser = createTestUser()
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithId(id: String): SafeUser? {
+                assertEquals(expectedUser.userId, id)
+                return null
+            }
+        }
+
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.ID(expectedUser.userId))
+        val response = exchangeRequest(request)
+
+        assert(response.result is DescribeUserResponse.Result.UserNotFound)
+
+        Unit
     }
 
     @Test
-    @Disabled("NOT Implemented")
-    fun process_username_muti() {
-        fail<Unit>("NOT Implemented")
+    fun describe_user_by_username_single() = runBlocking {
+        val expectedUser = createTestUser()
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithName(name: String): List<SafeUser> {
+                assertEquals(expectedUser.userName, name)
+                return listOf(expectedUser)
+            }
+        }
+
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.UserName(expectedUser.userName))
+        val response = exchangeRequest(request)
+
+        val result = response.result as DescribeUserResponse.Result.MultiUserResult
+        val users = result.multiUserResult.result
+
+        assert(users.size == 1)
+        val user = users.first()
+
+        user.assertEqualsTo(expectedUser)
+
+        Unit
+    }
+
+    @Test
+    fun describe_user_by_username_muti() = runBlocking {
+        val expectedName = "super fancy user name"
+        val inputUsers = (1..20).map {
+            SafeUser(
+                genRandomString(5),
+                expectedName,
+                genRandomString(10),
+                genRandomString(5)
+            )
+        }
+        val expectedUser = inputUsers.map(SafeUser::transform)
+
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithName(name: String): List<SafeUser> {
+                assertEquals(expectedName, name)
+                return inputUsers
+            }
+        }
+
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.UserName(expectedName))
+        val response = exchangeRequest(request)
+
+        val result = response.result as DescribeUserResponse.Result.MultiUserResult
+        val users = result.multiUserResult.result
+
+        assertEquals(expectedUser, users)
+
+        Unit
+    }
+
+    @Test
+    fun describe_user_by_username_none() = runBlocking {
+        val expectedUserName = "oh no there's no such user"
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithName(name: String): List<SafeUser> {
+                assertEquals(expectedUserName, name)
+                return listOf()
+            }
+        }
+
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.UserName(expectedUserName))
+        val response = exchangeRequest(request)
+
+        assert(response.result is DescribeUserResponse.Result.UserNotFound)
+
+        Unit
+    }
+
+    @Test
+    fun describe_user_db_exception() = runBlocking {
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithId(id: String): SafeUser? {
+                throw Exception("OOOOOOOOOOOOO NO")
+            }
+        }
+
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.ID(genRandomString(5)))
+        val response = exchangeRequest(request)
+
+        val error = response.result as DescribeUserResponse.Result.SystemErrorMessage
+        assertEquals("database error", error.systemErrorMessage)
+
+        Unit
+    }
+
+    @Test
+    fun describe_user_db_error() = runBlocking {
+        val dbAccess = object : TestDBAccess() {
+            override suspend fun getUserWithId(id: String): SafeUser? {
+                throw Error("OOOOOOOOOOOOO NO")
+            }
+        }
+
+        prepareAndRegistryInterface(dbAccess)
+
+        val request = DescribeUserRequest(DescribeUserRequest.By.ID(genRandomString(5)))
+        val response = exchangeRequest(request)
+
+        val error = response.result as DescribeUserResponse.Result.SystemErrorMessage
+        assertEquals("unexpected error", error.systemErrorMessage)
+
+        Unit
+    }
+
+    private fun createTestUser(): SafeUser {
+        return SafeUser(
+            userId = "userId-xxjkadfjakdjlf-${genRandomString(5)}",
+            userName = "super user name-${genRandomString(5)}",
+            signature = "lalala signature-${genRandomString(5)}",
+            avatarKey = "avatar-${genRandomString(5)}"
+        )
+    }
+
+    private suspend fun exchangeRequest(request: DescribeUserRequest): DescribeUserResponse {
+        val response = eventBus.requestAwait<ByteArray>(eventBusAddress, request.protoMarshal())
+        return DescribeUserResponse.protoUnmarshal(response.body())
+    }
+
+    private lateinit var target: DescribeUserInterface
+
+    private suspend fun prepareAndRegistryInterface(dbAccess: DBAccess) {
+        val config = Config { addSpec(MasterConfigSpec.DescribeUserConfig) }
+            .from.map.kv(mapOf("describe_user.event_bus_name" to eventBusAddress))
+
+        val sharedRuntimeData = SharedRuntimeData(vertx, config, dbAccess)
+        val target = DescribeUserInterface(sharedRuntimeData)
+        initializeAndRegistryTarget(target)
+    }
+
+    private suspend fun initializeAndRegistryTarget(target: DescribeUserInterface) {
+        target.bind(eventBus)
+        this.target = target
     }
 
     private fun com.kcibald.services.user.proto.User.assertEqualsTo(user: SafeUser) {
